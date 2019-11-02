52cd9f1c07ebb3c00613bbf103de6bee0127c4a3
sejin.eom <sejin1031@naver.com>
2019-11-02 13:44:22 +0900

    design about maincontainer

--- src/container/main/MainContainer.js
+++ src/container/main/MainContainer.js
@@ -5,7 +5,20 @@ class MainContainer extends Component {
   render() {
     return (
       <div className="main">
-        helloworld
+        <div className="content">
+          <div className="popout">
+            <span>W</span>
+            <span>E</span>
+            <span>L</span>
+            <span>C</span>
+            <span>O</span>
+            <span>M</span>
+            <span>E</span>
+           </div>
+          
+          <img src={require("./벌레.gif")} alt="bug"/>
+
+        </div>
       </div>
     );
   }

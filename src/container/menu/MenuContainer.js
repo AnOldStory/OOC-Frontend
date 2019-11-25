3db56891331235257a5c88c61d53a524bb218c74
sejin.eom <sejin1031@naver.com>
2019-11-25 20:59:36 +0900

    add menu design

--- src/container/menu/MenuContainer.js
+++ src/container/menu/MenuContainer.js
@@ -7,29 +7,29 @@ class MenuContainer extends Component {
   render() {
     return (
       <div className="menu">
-        <ul>
-          <li>
+        
+          
           <Link className="link" to="/">
             <div className="menuItem">Home</div>
-            </Link></li>
-            <li>
+            </Link>
+            
           <Link className="link" to="/people">
             <div className="menuItem">인사</div>
-            </Link></li>
-            <li>
+            </Link>
+            
           <Link className="link" to="/profit">
             <div className="menuItem">재무</div>
-            </Link></li>
-            <li>
+            </Link>
+            
           <Link className="link" to="/stock">
             <div className="menuItem">재고</div>
-            </Link></li>
-            <li>
+            </Link>
+            
           <Link className="link" to="/ticket">
             <div className="menuItem">티켓</div>
-            </Link></li>
+            </Link>
 
-        </ul>
+        
       </div>
     );
   }
